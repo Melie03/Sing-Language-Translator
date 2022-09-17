e55591eca2fc8e0f9f50048d1f213e4deb147394
Asmae Tounsi <asmaetounsi1@gmail.com>
2022-09-17 17:32:08 +0200

    data

--- Training/DataFlair_trainCNN.py
+++ Training/DataFlair_trainCNN.py
@@ -1,10 +1,10 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D, Dropout
-from keras.optimizers import Adam, SGD
-from keras.metrics import categorical_crossentropy
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D, Dropout
+from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import itertools
 import random
 import warnings
@@ -15,8 +15,8 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-train_path = r'D:\gesture\train'
-test_path = r'D:\gesture\test'
+train_path = r"/Users/atounsi/Documents/GitHub/Sing-Language-Translator/Data/Train"
+test_path = r"/Users/atounsi/Documents/GitHub/Sing-Language-Translator/Data/Test"
 
 train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=train_path, target_size=(64,64), class_mode='categorical', batch_size=10,shuffle=True)
 test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=test_path, target_size=(64,64), class_mode='categorical', batch_size=10, shuffle=True)
@@ -29,6 +29,7 @@ def plotImages(images_arr):
     fig, axes = plt.subplots(1, 10, figsize=(30,20))
     axes = axes.flatten()
     for img, ax in zip( images_arr, axes):
+        print(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         ax.imshow(img)
         ax.axis('off')
@@ -36,10 +37,10 @@ def plotImages(images_arr):
     plt.show()
 
 
-plotImages(imgs)
+#plotImages(imgs)
 print(imgs.shape)
 print(labels)
-'''
+
 model = Sequential()
 
 model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(64,64,3)))
@@ -58,10 +59,7 @@ model.add(Dense(128,activation ="relu"))
 #model.add(Dropout(0.2))
 model.add(Dense(128,activation ="relu"))
 #model.add(Dropout(0.3))
-model.add(Dense(10,activation ="softmax"))
-
-
-# In[23]:
+model.add(Dense(27,activation ="softmax"))
 
 
 model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
@@ -69,26 +67,30 @@ reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=1, min_lr
 early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
 
 
-
 model.compile(optimizer=SGD(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=1, min_lr=0.0005)
 early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
 
-
-history2 = model.fit(train_batches, epochs=10, callbacks=[reduce_lr, early_stop],  validation_data = test_batches)#, checkpoint])
+history2 = model.fit(train_batches, epochs=1, callbacks=[reduce_lr, early_stop],  validation_data = test_batches)#, checkpoint])
 imgs, labels = next(train_batches) # For getting next batch of imgs...
 
 imgs, labels = next(test_batches) # For getting next batch of imgs...
 scores = model.evaluate(imgs, labels, verbose=0)
+#print(sc)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+print(model.metrics_names[0])
+print(scores[0])
+print(model.metrics_names[1])
 print(f'{model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
-
 
 #model.save('best_model_dataflair.h5')
 model.save('best_model_dataflair3.h5')
 
 print(history2.history)
 
+'''
 imgs, labels = next(test_batches)
+
 
 model = keras.models.load_model(r"best_model_dataflair3.h5")
 
